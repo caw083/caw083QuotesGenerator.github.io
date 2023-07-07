ea9f6246b059240dac2afd384555cca14823b354
Christopher <christotopher083@gmail.com>
2023-07-07 14:07:10 +0700

    commit the quotes generator

---
+++ README.md
@@ -0,0 +1,2 @@
+### Qoute Generator
+this project use an API to build a random quote generator with Twitter integration.
--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-
-nextqoute = document.getElementById('new qoutes');
-text = document.getElementById('quote');
-author = document.getElementById('creator')
-twitter = document.getElementById('twitter');
+const nextQoute = document.getElementById('new qoutes');
+const text = document.getElementById('quote');
+const author = document.getElementById('creator')
+const twitter = document.getElementById('twitter');
+const loader = document.getElementsByClassName('loader')[0];
+const quoteContainer = document.getElementsByClassName('container')[0];
 
 let apiQuotes = []
 // get qoutes from API
 async function getQuotes(){
-    const url = "https://type.fit/api/quotes";
+    const url = "https://raw.githubusercontent.com/caw083/Quotes-Generator/main/quotes.json";
     try{
         const response = await fetch(url);
         apiQuotes = await response.json();
+        loading()
+        newQuote()
     }catch(error){
         alert(error);
     }
+}
+
+function loading(){
+    loader.hidden = true;
+    quoteContainer.style.display = "block"
 }
 
 function getRandomQuotes(){
@@ -23,10 +31,25 @@ function getRandomQuotes(){
 function newQuote(){
     randomQuotes = getRandomQuotes()
     text.innerHTML = randomQuotes['text']
-    author.innerHTML = randomQuotes['author']
+    if (!randomQuotes.author){
+        author.textContent = "Unknown"
+    } else {
+       author.innerHTML = randomQuotes.author
+    }
+    if (randomQuotes.text.length > 100){
+        text.classList.add('long-qoute')
+    }
+    else {
+        text.classList.remove('long-qoute')
+    }
 }
+
+function tweetQuotes(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${text.innerText} - ${author.innerText}`;
+    window.open(twitterUrl, '_blank')
+}
+
+nextQoute.addEventListener('click', newQuote)
+twitter.addEventListener('click', tweetQuotes)
+
 getQuotes();
-
-nextqoute.addEventListener('click', newQuote)
-
-
